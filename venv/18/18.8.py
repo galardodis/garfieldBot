4fddb426773be8a9267e8536ea447fc5d8e79076
galardodis <ilyla_dis@mail.ru>
2023-06-13 20:41:20 +0300

    Changed

---
+++ venv/18/18.8.py
@@ -0,0 +1,19 @@
+cost_min = 990
+cost_max = 1390
+
+def conference_cost():
+    total_prise = int()
+    number_of_persons = int(input('Сколько вам нужно билетов? '))
+    for i in range(number_of_persons):
+        age = int(input(f'Сколько лет участнику №{i+1}? '))
+        if age < 18:
+            total_prise += 0
+        elif 18 <= age < 25:
+            total_prise += cost_min
+        else:
+            total_prise += cost_max
+    if number_of_persons > 3:
+        total_prise = total_prise - total_prise * 0.1
+    return f'Итого к оплате {total_prise}р.'
+
+print(conference_cost())
