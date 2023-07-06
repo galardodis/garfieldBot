0d1787f0e1241a281ea8c0ce9001c6a02728b60f
galardodis <ilyla_dis@mail.ru>
2023-07-06 13:09:46 +0300

    Changed

--- venv/20/20.4.2.py
+++ venv/20/20.4.2.py
@@ -1,20 +1,50 @@
-file = input('Введите имя файла: ')
-alf = 'abcdefghijklmnopqrstuvwxyz'
-ans = dict()
+import json
 
-with open(file + '.txt', 'r', encoding='utf8') as f:
-    text = f.read().lower()
+ch = {}
+with open('json_example_QAP.json', 'r', encoding='utf8') as f:
+    asns = json.load(f)
 
-for i in text.split():
-    if i[0] not in alf:
-        continue
-    if not i.isalpha():
-        while not i[len(i) - 1] in alf:
-            i = i[:len(i) - 1]
-    if len(i) <= 3 or i in ans:
-        continue
-    else:
-        ans[i] = text.count(i)
+with open('list_of_checks.txt', 'r', encoding='utf8') as f:
+    for i in f.readlines():
+        i = i.split(':')
+        ch[i[0].strip()] = i[1].strip()
 
-print(max(ans, key=ans.get))
-print(max(ans.keys(), key=len))
+
+def checks(obj, file):
+    count = 0
+    for i in obj:
+        count += 1
+        test = 'Pass'
+        for key, value in file.items():
+            if len(i) != len(file):
+                print(f'Ответ {count}: количество строк ответа {len(i)}, должно быть {len(file)}!')
+                test = 'Failed'
+            if key not in i.keys():
+                print(f'Поля {key} нет в ответе')
+                test = 'Failed'
+            if key == 'eventType':
+                if not (i[key] == 'itemBuyEvent' or i[key] == 'itemViewEvent'):
+                    print(f'Ответ {count}, строка {key} не содержит itemBuyEvent или itemViewEvent')
+                    test = 'Failed'
+            if value == 'int':
+                if not type(i[key]) == int:
+                    print(f'Ответ {count}, строка {key} состоит не только из цифр')
+                    test = 'Failed'
+                continue
+            if value == 'bool':
+                if not type(i[key]) == bool:
+                    print(f'Ответ {count}, строка {key} не содержит Булево значение')
+                    test = 'Failed'
+                continue
+            if 'string' in value:
+                if 'url' in value:
+                    if not (i[key].startswith('http://') or i[key].startswith('https://')):
+                        print(f'Ответ {count}, строка {key} не содержит ссылку')
+                        test = 'Failed'
+                if not type(i[key]) == str:
+                    print(f'Ответ {count}, строка {key} не содержит строку')
+                    test = 'Failed'
+        print('Test -', test)
+
+
+checks(asns, ch)
