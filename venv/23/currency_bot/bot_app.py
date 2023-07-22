593ac108bff7c2e14590fcccb81821b810113185
galardodis <ilyla_dis@mail.ru>
2023-07-22 19:33:29 +0300

    new

---
+++ venv/23/currency_bot/bot_app.py
@@ -0,0 +1,55 @@
+import telebot
+from tokens import TELE_TOKEN
+from extensions import CryptoConverter, APIException, ls
+
+bot = telebot.TeleBot(TELE_TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите мне команду в формате: \n\n' \
+           '<имя валюты для перевода>  ' \
+           '<имя валюты в кототую нужно перевести>  ' \
+           '<количество переводимой валюты>' \
+           '\n\nПример ввода: Доллар Рубль 10' \
+           '\n\nДля получения списка валют введите: /values'
+    bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(commands=['values'])
+def help(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in ls.keys():
+        text = '\n'.join((text, key))
+    bot.send_message(message.chat.id, text)
+
+@bot.message_handler(commands=[])
+def help(message: telebot.types.Message):
+    raise APIException('Я не знаю такой команды')
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) > 3:
+            raise APIException('Слишком много параметров :( '
+                               '\n\nПопробуй ешё раз\nПример ввода: Доллар Рубль 10')
+
+        elif len(values) < 3:
+            raise APIException('Слишком мало параметров :( '
+                               '\n\nПопробуй ешё раз\nПример ввода: Доллар Рубль 10')
+
+        quote, base, amount = map(str.capitalize, values)
+        total_base = CryptoConverter.get_price(quote, base, amount)
+    except APIException as e:
+        bot.reply_to(message, f'Ты ошибся с вводом\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду \n{e}')
+    else:
+        text = f'{amount} {quote} равен {total_base} {base}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
---
+++ venv/23/currency_bot/extensions.py
@@ -0,0 +1,48 @@
+import requests
+import json
+
+
+ls = {
+    'Доллар': 'USD',
+    'Рубль': 'RUB',
+    'Евро': 'EUR',
+    'Юань': 'CNY',
+    'Лира': 'TRY',
+    'Гривна': 'UAH'
+}
+
+
+class APIException(Exception):
+    pass
+
+
+class CryptoConverter:
+
+    def all_currency():
+        pass
+    @staticmethod
+    def get_price(quote=str, base=str, amount=str):
+        try:
+            quote_ticker = ls[quote]
+        except KeyError:
+            raise APIException(f'Я не знаю волюту "{quote}"'
+                               f'\n\nПопробуй ешё раз\nПример ввода: Доллар Рубль 10')
+
+        try:
+            base_ticket = ls[base]
+        except KeyError:
+            raise APIException(f'Я не знаю волюту "{base}" '
+                               f'\n\nПопробуй ешё раз\nПример ввода: Доллар Рубль 10')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Мне кажется "{amount}" - это не цифра'
+                               f'\n\nПопробуй ешё раз\nПример ввода: Доллар Рубль 10')
+
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковае валюты {quote}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticket}')
+        total_base = round(json.loads(r.content)[base_ticket] * amount, 2)
+        return total_base
